3ef654b41c5379da8e50f0d5bf7bf5df98a113b5
snaydenov <stoyan.naydenov@gmail.com>
2014-02-22 05:19:03 +0200

    Sanity slipping....

--- lib/search/search.rb
+++ lib/search/search.rb
@@ -13,10 +13,21 @@ end
     @result = provider.perform_search(@query)
     save_search
   end
-
+  
+  def levenshtein_difference(a, b)
+    case
+      when a.empty? then b.length
+      when b.empty? then a.length
+      else [(a[0] == b[0] ? 0 : 1) + levenshtein_difference(a[1..-1], b[1..-1]),
+            1 + levenshtein_difference(a[1..-1], b),
+            1 + levenshtein_difference(a, b[1..-1])].min
+    end
+  end
+  
   def find_best_match
   
   end
+  
   
   class YoutubeSearch
     def initialize
